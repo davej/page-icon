97ad51d1e9b2e0f5de18db851485ac6f16d094cc
DaveJ <dave@davejeffery.com>
2018-10-19 16:59:10 +0100

    Add user-agent

--- src/modules/download/downloadIcon.js
+++ src/modules/download/downloadIcon.js
@@ -15,8 +15,9 @@ function downloadIcon(iconUrl) {
     const iconData = new Promise(function(resolve, reject) {
         axios
             .get(iconUrl, {
-                responseType: 'arraybuffer'
-                //'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+                responseType: 'arraybuffer',
+                'User-Agent':
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
             })
             .then(function(response) {
                 resolve(response.data);
--- src/modules/getPage.js
+++ src/modules/getPage.js
@@ -4,7 +4,9 @@ function getPage(pageUrl) {
     return new Promise(function(resolve, reject) {
         axios
             .get(pageUrl, {
-                timeout: 5000
+                timeout: 5000,
+                'User-Agent':
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
             })
             .then(function(response) {
                 const doesBlockFrame = 'x-frame-options' in response.headers;
