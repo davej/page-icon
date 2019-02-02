5a755c47472cbdf312c2b9db20b7323bb513a68b
DaveJ <dave@davejeffery.com>
2019-02-02 20:22:54 +0000

    increase timeout to 7.5s

--- src/modules/getPage.js
+++ src/modules/getPage.js
@@ -4,7 +4,7 @@ function getPage(pageUrl) {
     return new Promise(function(resolve, reject) {
         axios
             .get(pageUrl, {
-                timeout: 5000,
+                timeout: 7500,
                 'User-Agent':
                     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
             })
